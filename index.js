4afc105aba604e39a63942f03053701d2d64394e
Your Name <you@example.com>
2025-03-06 23:52:13 +0600

    Secure admin routes with JWT middleware

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb')
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -32,7 +33,31 @@ async function run() {
     const reveiwCollection = client.db('foodHub').collection('reveiws')
     const cartCollection = client.db('foodHub').collection('carts')
     const userCollection = client.db('foodHub').collection('users')
- 
+
+     // jwt related api
+     app.post('/jwt', async(req,res)=>{
+      const user  = req.body;
+      const  token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn: '365d'})
+      res.send({token})
+    })
+    // middleware 
+    const verifyToken = (req,res,next) =>{
+      console.log('inside verify token', req.headers.authorization);
+      if(!req.headers.authorization){
+        return res.status(401).send({message:'forbidden access'})
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded) =>{
+        if(err){
+          return res.status(401).send({message:'forbidden access'})
+        }
+        req.decoded = decoded;
+          next()
+        
+      })
+     
+    }
+  
     app.get('/menu',async(req,res)=>{
         const result =  await menuCollection.find().toArray()
         res.send(result)
@@ -49,13 +74,44 @@ async function run() {
       const result = await cartCollection.find(query).toArray()
       res.send(result)
     })
-    // cart coolection 
+   
+    // cart related api
     app.post('/carts',async(req,res)=>{
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem) 
       res.send(result)
     })
+    // use verify admin after verifyToken 
+    const verifyAdmin = async (req,res,next) =>{
+      const email = req.decoded.email;
+      const query = {email: email};
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if(!isAdmin){
+        return res.status(403).send({message: 'forbidden access'})
+      }
+      next()
+    }
     // user related api
+  app.get('/users', verifyToken, verifyAdmin, async(req,res)=>{
+    const result = await userCollection.find().toArray()
+    res.send(result)
+  })
+  app.get('/users/admin/:email',  verifyToken, async(req,res)=>{
+    const email = req.params.email;
+    if(email !== req.decoded.email
+    ){
+      return res.status(403).send({message:'unauthorized access'})
+    }
+    const query = {email: email};
+    const user = await userCollection.findOne(query)
+    let admin = false;
+    if(user){
+      admin = user?.role
+    }
+    res.send({admin});
+  })
+   
     app.post('/users',async(req,res)=>{
       const user = req.body;
       // insert email if user doesnot exists:
@@ -66,6 +122,29 @@ async function run() {
         return res.send({message:'user already exists', insertedId: null})
       }
       const result = await userCollection.insertOne(user)
+      res.send(result)
+    })
+    app.patch('/users/admin/:id', verifyToken,verifyAdmin, async(req,res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc = {
+        $set:{
+          role:'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter,updatedDoc)
+      res.send(result)
+    })
+    app.delete('/carts/:id',async(req,res)=>{
+      const id = req.params.id ;
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query)
+      res.send(result)
+    })
+    app.delete('/users/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query)
       res.send(result)
     })
     // Send a ping to confirm a successful connection
