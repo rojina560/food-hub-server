84d0816097b8ee6ae3563a9979c1dac8da8035bb
Your Name <you@example.com>
2025-03-24 15:40:24 +0600

    implement admin states

--- index.js
+++ index.js
@@ -212,6 +212,30 @@ app.post('/payments',async(req,res)=>{
   
   res.send({paymentResult,deleteResult})
 })
+app.get('/admin-stats', verifyToken, verifyAdmin,async(req,res)=>{
+  const users = await userCollection.estimatedDocumentCount();
+  const menuItems = await menuCollection.estimatedDocumentCount();
+  const orders = await paymentsCollection.estimatedDocumentCount();
+  // this is not the best way
+  //const payments = await paymentsCollection.find().toArray();
+ // const revenue = payments.reduce((total,payment)=> total + payment.price,0)
+ const result = await paymentsCollection.aggregate([{
+  $group:{
+    _id: null,
+    totalRevenue:{
+      $sum: '$price'
+    }
+  }
+ }
+ 
+
+ ]).toArray();
+ const revenue = result.length > 0 ? result[0].totalRevenue : 0;
+
+  res.send({
+    users,menuItems,orders,revenue
+  })
+})
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
